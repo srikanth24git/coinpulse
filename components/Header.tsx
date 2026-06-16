2d082782aa0e24e98d23ec8976ced4b083cd00b4
srikanth24git <k.srikanth2407@gmail.com>
2026-06-16 21:31:56 +0530

    implement navigation

---
+++ components/Header.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+
+const Header = () => {
+
+    const pathname = usePathname();
+  return (
+    <header>
+      <div className="main-container inner">
+        <Link href="/">
+          <Image src="logo.svg" alt="CoinPulse Logo" width={132} height={40} />
+        </Link>
+
+        <nav>
+          <Link href="/" className={cn('nav-link', { 'is-active': pathname === '/', 'is-home':true })}>
+            Home
+          </Link>
+          <p>Search Modal</p>
+          <Link href="/coins">All Coins</Link>
+        </nav>
+      </div>
+    </header>
+  );
+};
+export default Header;
